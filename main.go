3c2f11d4fc35d63e091f5d64c93c71f7dab102fc
Fedy <gayratovfirdavs720@gmail.com>
2024-04-16 21:41:38 +0500

    finish

--- main.go
+++ main.go
@@ -1,11 +1,42 @@
 package main
 
-type User struct{
-	Name string
-	Age uint8
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type User struct {
+	Name       string
+	Age        string
 	Occupation string
 }
 
-func main()  {
-	
+func main() {
+	f, err := os.Open("sample.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	var user []User
+	scanner := bufio.NewScanner(f)
+	var human User
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := strings.Split(line, ":")
+		switch words[0] {
+		case "Name":
+			human.Name = words[1]
+		case "Age":
+			human.Age = words[1]
+		case "Occupation":
+			human.Occupation = words[1]
+			user = append(user, human)
+		}
+	}
+	for _, i := range user {
+		fmt.Println(i)
+	}
 }
